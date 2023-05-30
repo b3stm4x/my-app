b5ea43d743ad1b4c055c8d95b35e77683f8336f2
Max Hog <maxi.hogrebe@googlemail.com>
2023-05-30 10:13:34 +0200

    Name bug undo

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Bean;
  *
  */
 @SpringBootApplication
-@Theme(value = "Duce Logistik AG")
+@Theme(value = "myapp")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
